4c253213e2976c7464a92b96bd4160a40ff806d9
ctadeodev <ctadeo.dev@gmail.com>
2024-06-16 23:21:21 +0800

    Updated vote simulation to align with expected voters turnout

--- scripts/vote_consumer.py
+++ scripts/vote_consumer.py
@@ -4,17 +4,8 @@ import logging
 import psycopg2
 from kafka import KafkaConsumer
 
-from config import VOTERS_TURNOUT
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-
-def set_voter_turnout_counts_per_state(ratio):
-    voter_counts = {}
-    for state, (_, voters_turnout, _) in VOTERS_TURNOUT.items():
-        voter_counts[state] = int(voters_turnout * ratio)
-    return voter_counts
 
 
 def consume_voting_events():
@@ -32,23 +23,15 @@ def consume_voting_events():
         port="5432"
     )
     cur = conn.cursor()
-    voter_turnout = set_voter_turnout_counts_per_state(1 / 10000)
     try:
         # Consume messages from Kafka topic indefinitely
         for message in consumer:
-            if not voter_turnout:
-                logger.info("Voter turnout reached")
-                break
             voting_event = message.value
 
             # Extract data from voting event message
             voter_id = voting_event['voter_id']
             candidate_id = voting_event['candidate_id']
             election_id = voting_event['election_id']
-            state = voting_event['state']
-            if state not in voter_turnout:
-                continue
-
             # Store the vote in the PostgreSQL database
             try:
                 cur.execute("""
@@ -62,17 +45,12 @@ def consume_voting_events():
             except psycopg2.Error as e:
                 conn.rollback()
                 logging.error("Error storing vote: %s", str(e))
-            else:
-                voter_turnout[state] -= 1
-                if not voter_turnout[state]:
-                    voter_turnout.pop(state)
     except KeyboardInterrupt:
         pass
     finally:
         # Close Kafka consumer
         print('Finished consuming')
         consumer.close()
-
         # Close PostgreSQL cursor and connection
         cur.close()
         conn.close()
--- scripts/vote_producer.py
+++ scripts/vote_producer.py
@@ -1,3 +1,4 @@
+import time
 import json
 import random
 import logging
@@ -11,24 +12,26 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def set_voter_turnout_counts_per_state(ratio):
+def set_expected_voters_turnout(ratio):
     voter_counts = {}
     for state, (_, voters_turnout, _) in VOTERS_TURNOUT.items():
         voter_counts[state] = int(voters_turnout * ratio)
     return voter_counts
 
 
-def update_voter_turnout_counts_per_state(expected_voter_turnout, actual_voters_turnout):
+def get_pending_voters(expected_voter_turnout, current_voters_turnout):
+    pending_voters_count = {}
     for state in expected_voter_turnout:
-        if state not in actual_voters_turnout:
+        if state not in current_voters_turnout:
+            pending_voters_count[state] = expected_voter_turnout[state]
             continue
-        expected_voter_turnout[state] -= actual_voters_turnout[state]
-        if expected_voter_turnout[state] < 1:
-            expected_voter_turnout.pop(state)
-    return expected_voter_turnout
+        pending_voters_count[state] = expected_voter_turnout[state] - current_voters_turnout[state]
+        if pending_voters_count[state] < 1:
+            pending_voters_count.pop(state)
+    return pending_voters_count
 
 
-def get_actual_voters_turnout(cur, election_id):
+def get_current_voters_turnout(cur, election_id):
     cur.execute("""
         SELECT
             v2.state,
@@ -66,44 +69,57 @@ def produce_voting_events(election_id, batch_size=200):
         bootstrap_servers=['localhost:9092'],
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
+
+    expected_voter_turnout = set_expected_voters_turnout(1 / 10000)
+    offset = 0
     try:
-        offset = 0
         while True:
+            pending_voters_count = get_pending_voters(
+                expected_voter_turnout, 
+                get_current_voters_turnout(cur, election_id)
+            )
+            if not pending_voters_count:
+                break
             # Query to fetch voters not yet voted in the specified election_id
-            # cur.execute("""
-            #     SELECT
-            #         v.voter_id,
-            #         v.state
-            #     FROM voters v
-            #     LEFT JOIN votes vo ON v.voter_id = vo.voter_id AND vo.election_id = %s
-            #     WHERE vo.voter_id IS NULL
-            #       AND v.state IN %s
-            #     ORDER BY v.voter_id
-            # """, (election_id, tuple(voter_turnout)))
             cur.execute("""
-                SELECT voter_id, state
-                FROM voters
+                SELECT v.voter_id, v.state
+                FROM voters v
+                LEFT JOIN votes vo ON v.voter_id = vo.voter_id AND vo.election_id = %s
+                WHERE vo.voter_id IS NULL AND v.state IN %s
+                ORDER BY v.voter_id
                 LIMIT %s
                 OFFSET %s
-            """, (batch_size, offset))
-            offset += batch_size
-
+            """, (election_id, tuple(pending_voters_count), batch_size, offset))
             rows = cur.fetchall()
             if not rows:
                 logger.info("Stopped voting events. No more voters available")
                 break
+
+            if len(rows) < batch_size:
+                offset = 0
+            else:
+                offset += batch_size
+            
+
             for row in rows:
                 voter_id, state = row
+                if state in pending_voters_count:
+                    pending_voters_count[state] -= 1
+                    if not pending_voters_count[state]:
+                        pending_voters_count.pop(state)
+                else:
+                    continue
                 # Create voting event/message
                 event = {
                     'voter_id': voter_id,
                     'election_id': election_id,
-                    'state': state,
                     'candidate_id': select_candidate_weighted(candidate_ids, state)
                 }
-
                 producer.send('vote', value=event)
                 logger.info("Produced registration event: %s", event)
+                time.sleep(0.1)
+            
+            time.sleep(random.randint(3, 5))
     except KeyboardInterrupt:
         pass
     finally:
